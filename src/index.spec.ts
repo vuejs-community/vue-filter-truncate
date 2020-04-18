1ac7cd7f58229f6e82312b0942110cae0455bc4d
Ed Nikolenko <4426698+ednikolenko@users.noreply.github.com>
2020-04-19 03:17:20 +0400

    tests: add unit tests

---
+++ src/index.spec.ts
@@ -0,0 +1,13 @@
+import ava, { TestInterface } from 'ava';
+
+import { truncate } from './index';
+
+const test = ava as TestInterface<{}>;
+
+test('This is sample => This is sample', (t) => {
+  t.is(truncate('This is sample', 20), 'This is sample');
+});
+
+test('This is sample => This …', (t) => {
+  t.is(truncate('This is sample', 4), 'This …');
+});

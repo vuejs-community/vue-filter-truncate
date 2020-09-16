82161e3646321aefc570c01cb7c1714edd3250de
Eduard Nikolenko <mail@eduardnikolenko.com>
2020-09-16 20:11:07 +0300

    Fix build to CommonJS

--- rollup.config.js
+++ rollup.config.js
@@ -13,7 +13,7 @@ export default {
     {
       exports: 'named',
       file: 'dist/vue-filter-truncate.cjs.js',
-      format: 'system',
+      format: 'commonjs',
       sourcemap: true
     }
   ],
